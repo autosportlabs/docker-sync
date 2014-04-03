eaa83a044f12bb2f6f3872f2d47259b673e5eb8c
Brian Lalor <brian@autosportlabs.com>
2014-04-03 16:42:38 -0400

    One dot oh!  One dot oh!  One dot oh!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="docker-sync",
-    version="0.0.0",
+    version="1.0.0",
     description="Configuration management for Docker containers",
     long_description=open("README.md").read(),
     author="Brian Lalor",
