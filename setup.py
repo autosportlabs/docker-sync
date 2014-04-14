e0d3d7b822e3a03b28e3b25e79cd176a2be4548d
Brian Lalor <brian@autosportlabs.com>
2014-04-13 20:41:59 -0400

    Releasing v1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="docker-sync",
-    version="1.0.2",
+    version="1.0.3",
     description="Configuration management for Docker containers",
     long_description=open("README.md").read(),
     author="Brian Lalor",
