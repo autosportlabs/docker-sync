b9c9296dc79fd83ee460ae4f36373aef5aa69f54
Brian Lalor <brian@autosportlabs.com>
2014-11-23 21:12:51 -0500

    Rip out setuptools
    Too-specific requirements in other libraries are fucking with
    setuptools' automatic path manipulation.
    
    Traceback (most recent call last):
      File "/usr/bin/docker-sync", line 5, in <module>
        from pkg_resources import load_entry_point
      File "/usr/lib/python2.6/site-packages/pkg_resources.py", line 2659, in <module>
        parse_requirements(__requires__), Environment()
      File "/usr/lib/python2.6/site-packages/pkg_resources.py", line 546, in resolve
        raise DistributionNotFound(req)
    pkg_resources.DistributionNotFound: websocket-client==0.11.0
    
    Who fucking has time for this bullshit?

--- setup.py
+++ setup.py
@@ -1,33 +1,16 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
     name="docker-sync",
-    version="1.1.0",
+    version="1.2.0",
     description="Configuration management for Docker containers",
     long_description=open("README.md").read(),
     author="Brian Lalor",
     author_email="brian@autosportlabs.com",
     url="http://github.com/autosportlabs/docker-sync",
-    packages=find_packages(),
-    setup_requires=[
-        "nose >= 1.3.4, < 1.4.0",
-    ],
-    tests_require=[
-        "HTTPretty >= 0.8.3, < 0.9.0"
-    ],
-    install_requires=[
-        "PyYAML >= 3.10, < 4.0",
-        "argparse >= 1.1",
-        "docker-py >= 0.3.0, < 0.4.0",
-        "setuptools",
-        "semantic_version >= 2.3.1, < 2.4.0"
-    ],
-    entry_points={
-        "console_scripts": [
-            "docker-sync = docker_sync.cli:sync",
-        ],
-    },
+    packages=[ "docker_sync", "docker_sync.lib" ],
+    scripts=[ "scripts/docker-sync" ],
 )
