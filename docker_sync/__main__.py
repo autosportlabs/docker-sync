7ab096c39876d3d07f3d5ac25c71d5d9a24044a0
Brian Lalor <brian@autosportlabs.com>
2014-12-10 12:31:58 -0500

    Fix "python docker_sync/"

--- docker_sync/__main__.py
+++ docker_sync/__main__.py
@@ -4,5 +4,5 @@
 ## the sole purpose of this file is so that I can run "python docker_sync" in
 ## the root of the project.  I don't know how it works, but it does. :-)
 
-from __init__ import main
-main()
+from cli import sync
+sync()
