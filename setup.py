2519097ed9be9595782ebaad24c15eb4969000fb
Brian Lalor <brian@autosportlabs.com>
2014-12-10 12:33:38 -0500

    Bumping docker-py version
    Fuck packaging.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="docker-sync",
-    version="1.2.0",
+    version="1.2.1",
     description="Configuration management for Docker containers",
     long_description=open("README.md").read(),
     author="Brian Lalor",
